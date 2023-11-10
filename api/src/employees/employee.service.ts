5b2d7d1b2ec24598af6b77fb7eb1fc69199707e7
robport <26696668+robport@users.noreply.github.com>
2023-11-10 13:55:46 +0000

    fix

---
+++ api/src/employees/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Employee } from './employee.entity';
+import { CreateEmployeeDto, UpdateEmployeeDto } from './employee.types';
+
+@Injectable()
+export class EmployeeService {
+  constructor(
+    @InjectRepository(Employee)
+    private employeeRepository: Repository<Employee>
+  ) {
+  }
+
+  async findAll(): Promise<Employee[]> {
+    return this.employeeRepository.find();
+  }
+
+  async create(employee: CreateEmployeeDto): Promise<Employee> {
+    return this.employeeRepository.save(employee);
+  }
+
+  async update(employee: UpdateEmployeeDto): Promise<Employee> {
+    return this.employeeRepository.save(employee);
+  }
+
+  async delete(employeeId: number): Promise<void> {
+    await this.employeeRepository.delete({
+      id: employeeId
+    });
+  }
+}
